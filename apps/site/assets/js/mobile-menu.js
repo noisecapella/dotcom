713bb6d4f635aa6d5bcca6e0a78a837963a522e0
Hannah Purcell <hpurcell@mbta.com>
2021-11-09 10:21:53 -0500

    First pass of mobile menu (without tests)

--- apps/site/assets/js/mobile-menu.js
+++ apps/site/assets/js/mobile-menu.js
@@ -1,17 +1,7 @@
-// Responses handled here:
-// - "menu-open" or "search-open" added as class to header
-// - HTML body prevented from scrolling
-// - update aria-expanded to indicate menu state
-// - update text of menu toggle button
-// - pass focus from toggle button to first accordion item
-// - reset scroll on menu content when menu is closed
-// - menu can be closed by pressing esc key or veil-click
-import { handleNativeEscapeKeyPress } from "../ts/helpers/keyboard-events";
-
 export default function($ = window.jQuery) {
 
   function setupMobileMenu() {
-    const htmlElement = document.documentElement;
+    const htmlElement = document.getElementsByTagName("html")[0];
     const menuButton = document.querySelector("button.m-menu__toggle");
     const searchButton = document.querySelector("button.header-search__toggle");
     const veil = document.querySelector(".m-menu__cover");
@@ -24,64 +14,36 @@ export default function($ = window.jQuery) {
       }, 200);
     }
 
-    const closeMenus = () => {
-      menuButton.ariaExpanded = "false"
-      searchButton.ariaExpanded = "false"
-      menuButton.innerHTML = 'Menu'
-      $(header).removeClass('menu-open');
-      $(header).removeClass('search-open');
-      $(htmlElement).removeClass('modal-open');
-      scrollToTop();
-    }
-    
-    // Menu / search button is a toggle
     menuButton.addEventListener("click", () => {
       $(header).toggleClass('menu-open');
-      // Class prevents body scrolling
       $(htmlElement).toggleClass('modal-open');
-
-      const expanded = menuButton.getAttribute("aria-expanded") == 'true';
-      menuButton.setAttribute("aria-expanded", !expanded);
-      menuButton.innerHTML = expanded ? 'Menu' : 'Close';
-      // Pass focus
-      if (!expanded) {
-        const firstAccordion = document.querySelector(".accordion")
-        firstAccordion.focus();
-      }
       scrollToTop();
     });
     searchButton.addEventListener("click", () => {
       $(header).toggleClass('search-open');
       $(htmlElement).toggleClass('modal-open');
-
-      const expanded = searchButton.getAttribute("aria-expanded") == 'true';
-      searchButton.setAttribute("aria-expanded", !expanded);
-      // Pass focus
-      if (!expanded) {
-        const searchBar = document.querySelector(".m-menu__search #search-homepage__input")
-        searchBar.focus();
-      }
     });
 
-    // Veil click closes
     veil.addEventListener("click", () => {
-      closeMenus();
+      $(header).removeClass('menu-open');
+      $(header).removeClass('search-open');
+      $(htmlElement).removeClass('modal-open');
+      scrollToTop();
     })
 
-    // Esc key closes
     document.body.addEventListener("keydown", (e) => {
-      handleNativeEscapeKeyPress(e, closeMenus);
+      if (e.key == 'Escape') {
+        $(header).removeClass('menu-open');
+        $(header).removeClass('search-open');
+        $(htmlElement).removeClass('modal-open');
+        scrollToTop();
+      }
     })
   }
   
   document.addEventListener(
     "turbolinks:load",
-    () => {
-      const newMobileMenuToggle = document.querySelector("button.m-menu__toggle");
-      if (newMobileMenuToggle) {
-        setupMobileMenu($)
-      }
-    },
+    () => window.nextTick(setupMobileMenu),
     { passive: true }
   );
 }
